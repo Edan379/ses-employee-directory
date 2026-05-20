5d5772d8b554483c2760aa42da7ef2df6ad33035
jordan <lil_joh973@hotmail.fr>
2026-05-20 16:58:07 +0200

    chore: add global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //use ValidationPipes to apply input validation into each route handler via dto's class-validator (rules defined)
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true, // Si le body contient des propriétés non déclarées dans les dtos la requete est rejeté avec code 422
+    forbidUnknownValues: true, //dans le body si la valeur de la clé n'est pas attendue ou si l'objet est vide il lève une erreur
+    transform: true,
+    errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+    disableErrorMessages: process.env.NODE_ENV === 'production',
+  }));
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 3000;
   await app.listen(port ?? 3000);
